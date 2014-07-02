e13afca79f89d516b3316257acf7488c9e55b98c
resteve <resteve@zikzakmedia.com>
2014-07-02 13:32:54 +0200

    Initial commit

---
+++ invoice.py
@@ -0,0 +1,135 @@
+from flask import Blueprint, render_template, current_app, abort, g, \
+    url_for, request, session, send_file
+from galatea.tryton import tryton
+from galatea.utils import get_tryton_locale, slugify
+from galatea.helpers import login_required
+from flask.ext.babel import gettext as _
+from flask.ext.paginate import Pagination
+import tempfile
+
+invoice = Blueprint('invoice', __name__, template_folder='templates')
+
+DISPLAY_MSG = _('Displaying <b>{start} - {end}</b> {record_name} in total <b>{total}</b>')
+
+limit = current_app.config.get('TRYTON_PAGINATION_INVOICE_LIMIT', 20)
+invoice_report = current_app.config.get('TRYTON_INVOICE_REPORT', 'account.invoice')
+state_exclude = current_app.config.get('TRYTON_INVOICE_STATE_EXCLUDE')
+
+Invoice = tryton.pool.get('account.invoice')
+InvoiceReport = tryton.pool.get('account.invoice', type='report')
+
+INVOICE_FIELD_NAMES = [
+    'create_date', 'invoice_date', 'number', 'reference', 'description',
+    'state', 'type', 'untaxed_amount', 'tax_amount', 'total_amount',
+    ]
+
+@tryton.default_context
+def default_context():
+    context = {}
+    context['language'] = get_tryton_locale(g.language)
+    return context
+
+@invoice.route("/print/<id>", endpoint="invoice_print")
+@login_required
+@tryton.transaction()
+def invoice_print(lang, id):
+    '''Invoice Print'''
+
+    invoices = Invoice.search_read([
+        ('id', '=', id),
+        ('party', '=', session['customer']),
+        ('state', 'in', current_app.config['TRYTON_INVOICE_PRINT']),
+        ], limit=1, fields_names=['number'])
+    
+    if not invoices:
+        abort(404)
+
+    invoice, = invoices
+
+    _, report, _, _ = InvoiceReport.execute([invoice['id']], {})
+    report_name = 'invoice-%s.pdf' % (slugify(invoice.get('number')) or 'invoice')
+
+    with tempfile.NamedTemporaryFile(
+            prefix='%s-' % current_app.config['TRYTON_DATABASE'],
+            suffix='.pdf', delete=False) as temp:
+        temp.write(report)
+    temp.close()
+    data = open(temp.name, 'rb')
+
+    return send_file(data, attachment_filename=report_name, as_attachment=True)
+
+@invoice.route("/<id>", endpoint="invoice")
+@login_required
+@tryton.transaction()
+def invoice_detail(lang, id):
+    '''Invoice Detail'''
+
+    invoices = Invoice.search([
+        ('id', '=', id),
+        ('party', '=', session['customer']),
+        ('state', 'not in', state_exclude),
+        ], limit=1)
+    if not invoices:
+        abort(404)
+
+    invoice, = Invoice.browse(invoices)
+
+    #breadcumbs
+    breadcrumbs = [{
+        'slug': url_for('my-account', lang=g.language),
+        'name': _('My Account'),
+        }, {
+        'slug': url_for('.invoices', lang=g.language),
+        'name': _('Invoices'),
+        }, {
+        'slug': url_for('.invoice', lang=g.language, id=invoice.id),
+        'name': invoice.number or _('Not number'),
+        }]
+
+    return render_template('invoice.html',
+            breadcrumbs=breadcrumbs,
+            invoice=invoice,
+            )
+
+@invoice.route("/", endpoint="invoices")
+@login_required
+@tryton.transaction()
+def invoice_list(lang):
+    '''Invoices'''
+
+    try:
+        page = int(request.args.get('page', 1))
+    except ValueError:
+        page = 1
+
+    domain = [
+        ('party', '=', session['customer']),
+        ('state', 'not in', state_exclude),
+        ]
+    total = Invoice.search_count(domain)
+    offset = (page-1)*limit
+
+    order = [
+        ('invoice_date', 'DESC'),
+        ('id', 'DESC'),
+        ]
+    invoices = Invoice.search_read(
+        domain, offset, limit, order, INVOICE_FIELD_NAMES)
+
+    pagination = Pagination(
+        page=page, total=total, per_page=limit, display_msg=DISPLAY_MSG, bs_version='3')
+
+    #breadcumbs
+    breadcrumbs = [{
+        'slug': url_for('my-account', lang=g.language),
+        'name': _('My Account'),
+        }, {
+        'slug': url_for('.invoices', lang=g.language),
+        'name': _('Invoices'),
+        }]
+
+    return render_template('invoices.html',
+            breadcrumbs=breadcrumbs,
+            pagination=pagination,
+            invoices=invoices,
+            )
