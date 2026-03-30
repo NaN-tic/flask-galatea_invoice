3d99542f8c711ac07e78a799beebecc899528cf5
Raimon Esteve <raimonesteve@gmail.com>
2026-03-30 13:58:12 +0200

    Upgrade to flask3 #178864

--- invoice.py
+++ invoice.py
@@ -1,7 +1,7 @@
 from trytond.modules.voyager import slugify
 from flask import Blueprint, render_template, current_app, abort, g, \
     url_for, request, session, send_file
-from galatea.tryton import tryton
+from app_extensions import tryton
 from galatea.helpers import login_required, customer_required
 from flask_babel import gettext as _, lazy_gettext
 from flask_paginate import Pagination
@@ -11,13 +11,17 @@ invoice = Blueprint('invoice', __name__, template_folder='templates')
 
 DISPLAY_MSG = lazy_gettext('Displaying <b>{start} - {end}</b> of <b>{total}</b>')
 
-LIMIT = current_app.config.get('TRYTON_PAGINATION_INVOICE_LIMIT', 20)
-INVOICE_REPORT = current_app.config.get('TRYTON_INVOICE_REPORT', 'account.invoice')
-STATE_EXCLUDE = current_app.config.get('TRYTON_INVOICE_STATE_EXCLUDE', [])
-STATE_INVOICE_PRINT = current_app.config.get('TRYTON_INVOICE_PRINT', ['paid'])
+def _limit():
+    return current_app.config.get('TRYTON_PAGINATION_INVOICE_LIMIT', 20)
 
-Invoice = tryton.pool.get('account.invoice')
-InvoiceReport = tryton.pool.get(INVOICE_REPORT, type='report')
+
+def _state_exclude():
+    return current_app.config.get('TRYTON_INVOICE_STATE_EXCLUDE', [])
+
+
+def _state_invoice_print():
+    return current_app.config.get('TRYTON_INVOICE_PRINT', ['paid'])
+
 
 @invoice.route("/print/<int:id>", endpoint="invoice_print")
 @login_required
@@ -25,10 +29,13 @@ InvoiceReport = tryton.pool.get(INVOICE_REPORT, type='report')
 @tryton.transaction()
 def invoice_print(lang, id):
     '''Invoice Print'''
+    Invoice = tryton.pool.get('account.invoice')
+    report_name = current_app.config.get('TRYTON_INVOICE_REPORT', 'account.invoice')
+    InvoiceReport = tryton.pool.get(report_name, type='report')
 
     domain = [
         ('id', '=', id),
-        ('state', 'in', STATE_INVOICE_PRINT),
+        ('state', 'in', _state_invoice_print()),
         ]
     if not session.get('manager', False):
         domain.append(('party', '=', session['customer']))
@@ -57,6 +64,7 @@ def invoice_print(lang, id):
 @tryton.transaction()
 def invoice_detail(lang, id):
     '''Invoice Detail'''
+    Invoice = tryton.pool.get('account.invoice')
 
     invoices = Invoice.search([
         ('id', '=', id),
@@ -90,6 +98,8 @@ def invoice_detail(lang, id):
 @tryton.transaction()
 def invoice_list(lang):
     '''Invoices'''
+    Invoice = tryton.pool.get('account.invoice')
+    limit = _limit()
 
     try:
         page = int(request.args.get('page', 1))
@@ -98,19 +108,19 @@ def invoice_list(lang):
 
     domain = [
         ('party', '=', session['customer']),
-        ('state', 'not in', STATE_EXCLUDE),
+        ('state', 'not in', _state_exclude()),
         ]
     total = Invoice.search_count(domain)
-    offset = (page-1)*LIMIT
+    offset = (page-1)*limit
 
     order = [
         ('invoice_date', 'DESC'),
         ('id', 'DESC'),
         ]
-    invoices = Invoice.search(domain, offset, LIMIT, order)
+    invoices = Invoice.search(domain, offset, limit, order)
 
     pagination = Pagination(
-        page=page, total=total, per_page=LIMIT, display_msg=DISPLAY_MSG, bs_version='3')
+        page=page, total=total, per_page=limit, display_msg=DISPLAY_MSG, bs_version='3')
 
     #breadcumbs
     breadcrumbs = [{
